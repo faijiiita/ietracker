53e9bfb0f8e55e2013f99017b8a81043bc7cfd36
Faij Ali <faizali.iiita@gmail.com>
2025-08-24 13:08:47 +0530

    feat: add CreateTransactionDialogBox component and transaction schema for income and expense management

---
+++ app/(dashboard)/_components/create-transaction-dialogbox.tsx
@@ -0,0 +1,104 @@
+"use client";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { TransactionType } from "@/lib/types";
+import {
+  CreateTransactionSchema,
+  CreateTransactionSchemaType,
+} from "@/schema/transaction";
+import { ReactNode, useState } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { cn } from "@/lib/utils";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const CreateTransactionDialogBox = ({
+  trigger,
+  type,
+}: {
+  trigger: ReactNode;
+  type: TransactionType;
+}) => {
+  const [open, setOpen] = useState(false);
+
+  const form = useForm<CreateTransactionSchemaType>({
+    resolver: zodResolver(CreateTransactionSchema),
+    defaultValues: {
+      amount: 0,
+      description: "",
+      category: undefined,
+      type: type,
+      date: undefined,
+    },
+  });
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>{trigger}</DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>
+            Create a new{" "}
+            <span
+              className={cn(
+                "m-1",
+                type === "income" ? "text-emerald-500" : "text-red-500"
+              )}
+            >
+              {type.toUpperCase()}
+            </span>{" "}
+            Transaction
+          </DialogTitle>
+        </DialogHeader>
+        <Form {...form}>
+          <form className="space-y-4">
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Description</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    Transaction Description (Optional)
+                  </FormDescription>
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="amount"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Amount</FormLabel>
+                  <FormControl>
+                    <Input type="number" {...field} />
+                  </FormControl>
+                  <FormDescription>Transaction Amount</FormDescription>
+                </FormItem>
+              )}
+            />
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default CreateTransactionDialogBox;
--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,5 +1,57 @@
-const DashboardPage = () => {
-  return <div>DashboardPage</div>;
+import prisma from "@/lib/prisma";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import CreateTransactionDialogBox from "./_components/create-transaction-dialogbox";
+import { Button } from "@/components/ui/button";
+
+const DashboardPage = async () => {
+  const user = await currentUser();
+  if (!user) {
+    redirect("/sign-in");
+  }
+
+  const userSettings = await prisma.userSettings.findUnique({
+    where: {
+      userId: user.id,
+    },
+  });
+  if (!userSettings) {
+    redirect("/wizard");
+  }
+
+  return (
+    <div className="h-full bg-background">
+      <div className="border-b bg-card">
+        <div className="container flex flex-wrap items-center justify-between gap-6 p-8">
+          <p className="text-3xl font-bold">Hello, {user.firstName}! 👋</p>
+          <div className="flex items-center gap-3">
+            <CreateTransactionDialogBox
+              trigger={
+                <Button
+                  variant={"outline"}
+                  className="dark:border-emerald-500 dark:bg-emerald-950 hover:dark:bg-emerald-700 border-emerald-500 bg-emerald-700 hover:bg-emerald-500 text-white hover:text-white"
+                >
+                  New Income 🤑
+                </Button>
+              }
+              type="income"
+            />
+            <CreateTransactionDialogBox
+              trigger={
+                <Button
+                  variant={"outline"}
+                  className="dark:border-rose-500 dark:bg-rose-950 hover:dark:bg-rose-700 border-rose-500 bg-rose-700 hover:bg-rose-500 text-white hover:text-white"
+                >
+                  New Expense 😤
+                </Button>
+              }
+              type="expense"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default DashboardPage;
