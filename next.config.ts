af7191f868e7ef3650b6710d000a8498c05c638e
Faij Ali <faizali.iiita@gmail.com>
2025-09-28 22:45:03 +0530

    Allowing all api

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
   async headers() {
     return [
       {
-        source: "/:path*",
+        source: "/*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           {
